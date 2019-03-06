03e4dd4fffede9ecb1fe58337579a22371c126dd
hafiz <abdullahalhafiz77@gmail.com>
2019-03-06 17:08:36 +0600

    add generic view

--- app_dir/polls/api/urls.py
+++ app_dir/polls/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
 
-from .views import ( PollListCreate,PollRetriveUpdateDestroy)
+from .views import ( PollListCreate,PollRetriveUpdateDestroy,ChoiceListCreate)
 
 
 urlpatterns = [
     path('', PollListCreate.as_view(), name='polls-list'),
+    path('choice/', ChoiceListCreate.as_view(), name='choice-list'),
     path('<int:pk>/', PollRetriveUpdateDestroy.as_view(), name='pool-details'),
 
 ]
--- app_dir/polls/api/views.py
+++ app_dir/polls/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.generics import (
     ListAPIView, CreateAPIView, RetrieveUpdateAPIView, RetrieveUpdateDestroyAPIView,
-    RetrieveAPIView, DestroyAPIView, ListCreateAPIView,
+    RetrieveAPIView, DestroyAPIView, ListCreateAPIView, GenericAPIView
 )
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import pagination
+from rest_framework import pagination, status
 from django.db.models import Q
 
 from rest_framework.permissions import (
@@ -21,20 +22,12 @@ class PollListCreate(ListCreateAPIView):
 
     def get_queryset(self, *args, **kwargs):
         queryset_list = Choice.objects.all()
-
         page_size = 'page_size'
         if self.request.GET.get(page_size):
             pagination.PageNumberPagination.page_size = self.request.GET.get(
                 page_size)
         else:
             pagination.PageNumberPagination.page_size = 10
-        query = self.request.GET.get('q')
-        if query:
-            queryset_list = queryset_list.filter(
-                Q(email__icontains=query) |
-                Q(username__icontains=query)
-            )
-
         return queryset_list.order_by('-id')
 
 
@@ -43,3 +36,28 @@ class PollRetriveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PoolSerializer
     queryset = Choice.objects.all()
+
+
+class ChoiceListCreate(GenericAPIView):
+    """
+    List all Choice.
+    """
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PoolSerializer
+    pagination_class = PostLimitOffsetPagination
+    
+    def get(self, request, format=None, *args, **kwargs):
+        queryset_list = Choice.objects.all()
+        serializer = self.serializer_class(queryset_list, many=True, context={'request': request})
+        data = {'result': serializer.data}
+        return Response(data)
+        
+    # """
+    # Create Choice.
+    # """
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
